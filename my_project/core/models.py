855332d921210f9112125a2529d1ac22cabfe669
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-10-08 16:19:04 +0300

    new data added

--- my_project/core/models.py
+++ my_project/core/models.py
@@ -12,7 +12,7 @@ class Teacher(models.Model):
     name = models.CharField(max_length=200)
     age = models.PositiveIntegerField(default=0)
     email = models.EmailField(max_length=200)
-    group = models.ManyToManyField(Group)
+    group = models.ForeignKey("core.Group", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -22,7 +22,7 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     age = models.PositiveIntegerField(default=0)
     email = models.EmailField(max_length=200)
-    group = models.ManyToManyField(Group)
+    group = models.ForeignKey("core.Group", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
--- my_project/core/views.py
+++ my_project/core/views.py
@@ -9,20 +9,23 @@ class IndexTemplateView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexTemplateView, self).get_context_data(**kwargs)
         context['users'] = User.objects.all()
-        group = Group.object.all()
+        group = Group.objects.all()
         print(group)
-        teacher = Teacher.object.all()
+        teacher = Teacher.objects.all()
         print(teacher)
-        student = Student.object.all()
+        student = Student.objects.all()
         print(student)
-        student_in_group = Student.object.filter(group="#2")
+        student_in_group = Student.objects.filter(group__name__exact="#2")
         print(student_in_group)
-        teacher_students = Student.object.filter()
-        print(group)
-        student_age = Student.object.filter(age__gt=20)
+        first_teacher_group_id = Teacher.objects.first().group_id
+        teacher_students = Student.objects.filter(group=first_teacher_group_id)
+        print(teacher_students)
+        student_age = Student.objects.filter(age__gt=20)
         print(student_age)
-        student_email = Student.object.filter(email="gmail.com")
+        student_email = Student.objects.filter(email__contains="gmail.com")
         print(student_email)
+        teacher_students_age = Student.objects.filter(group=first_teacher_group_id, age__gt=20)
+        print(teacher_students_age)
         return context
 
 
