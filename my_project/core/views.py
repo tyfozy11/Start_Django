ee5be5ca34d69d02abe4b5176a914db4781675b1
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-10-18 00:24:14 +0300

    new data added

--- my_project/core/views.py
+++ my_project/core/views.py
@@ -1,16 +1,16 @@
-from django.views.generic import TemplateView, DetailView, ListView
-from core.models import Group, Teacher, Student, Courses, Category
+from django.views.generic import TemplateView, ListView
+from core.models import Courses, Category
 
 
 class IndexView(ListView):
     template_name = "index.html"
-    models = Courses
-    # paginate_by = 10
-    #
-    # def get_queryset(self):
-    #     queryset = super(IndexView, self).get_queryset()
-    #     return queryset.order_by("name").select_related(
-    #         "name_teacher")
+    model = Courses
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super(IndexView, self).get_queryset()
+        return queryset.order_by("name").select_related(
+            "name_teacher")
 
     def get_context_data(self, *args, **kwargs):
         context = super(IndexView, self).get_context_data(*args, **kwargs)
